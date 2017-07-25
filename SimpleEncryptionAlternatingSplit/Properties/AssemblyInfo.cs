e84df21de28500faee21d4ed408a057f6c92c84c
TitanDarren <sqz777isme@gmail.com>
2017-07-25 09:54:27 +0800

    initial project

---
+++ SimpleEncryptionAlternatingSplit/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("SimpleEncryptionAlternatingSplit")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Titansoft")]
+[assembly: AssemblyProduct("SimpleEncryptionAlternatingSplit")]
+[assembly: AssemblyCopyright("Copyright © Titansoft 2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("cf19860e-63ba-4027-8f8e-d42ad3446add")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
